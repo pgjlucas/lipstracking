84ec9f7ef56b04b53da7567b79ec8e9ebfea95f0
Pierre Lucas <65091079+pgjlucas@users.noreply.github.com>
2020-05-11 17:59:01 +0200

    Delete testAlgorithm.m

--- testAlgorithm.m
+++
@@ -1,9 +0,0 @@
-% Compute time complexity for the classifier3
-totalTime = [0,0,0] ;
-for i=6:6
-    filename =['/path/img/' num2str(i) '.jpg'];
-    timeComplexity=Classifier3(imread(filename));
-    totalTime = totalTime + timeComplexity;
-end
-
-averageTime = totalTime/25 ;
